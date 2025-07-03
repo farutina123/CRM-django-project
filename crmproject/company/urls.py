72e1631cc72979eaedfe41ab264cf50be0211fec
Полина <farutina97@gmail.com>
2025-07-03 18:17:08 +0300

    удаление компании через параметр

--- crmproject/company/urls.py
+++ crmproject/company/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import CreateCompanyView
+from .views import CreateCompanyView, DeleteCompanyView
 
 
 urlpatterns = [
-    path("create-company/", CreateCompanyView.as_view(), name="create-company")]
+    path("create/", CreateCompanyView.as_view(), name="create-company"),
+    path("delete/", DeleteCompanyView.as_view(), name="delete-company"),
+    path("delete/<int:{id}>/", DeleteCompanyView.as_view())]
 
--- crmproject/company/views.py
+++ crmproject/company/views.py
@@ -2,7 +2,7 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializers import CompanySerializer
-from drf_spectacular.utils import extend_schema
+from drf_spectacular.utils import extend_schema, OpenApiParameter, OpenApiTypes
 
 
 @extend_schema(
@@ -23,3 +23,16 @@ class CreateCompanyView(APIView):
         user.is_company_owner = True
         user.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+@extend_schema(
+    tags=['company'],
+    description="Удаление компании доступно только авторизованным пользователям",
+    parameters=[
+        OpenApiParameter(name='id', type=OpenApiTypes.INT, location=OpenApiParameter.PATH, description='ID')
+    ]
+)
+class DeleteCompanyView(APIView):
+    permission_classes = []
+    def delete(self, request, pk=None):
+        print(pk)

9938fe37fa3757a6d0cdd84ca317e44a41c64d31
Полина <farutina97@gmail.com>
2025-07-03 18:32:18 +0300

    удаление компании через параметр

--- crmproject/company/urls.py
+++ crmproject/company/urls.py
@@ -4,6 +4,5 @@ from .views import CreateCompanyView, DeleteCompanyView
 
 urlpatterns = [
     path("create/", CreateCompanyView.as_view(), name="create-company"),
-    path("delete/", DeleteCompanyView.as_view(), name="delete-company"),
-    path("delete/<int:{id}>/", DeleteCompanyView.as_view())]
+    path("delete/<int:id>/", DeleteCompanyView.as_view())]
 
